72881af67e594fce40b4f9057d2e30c4c1ecf707
Konrad Gorazd <konrad.gorazd@gmail.com>
2024-04-16 15:39:49 +0200

    Add missing DB indexes on companies table

---
+++ db/migrate/20240416145634_add_missing_indexes.rb
@@ -0,0 +1,8 @@
+class AddMissingIndexes < ActiveRecord::Migration[7.0]
+  def change
+    add_index :companies, :name
+    add_index :companies, :industry
+    add_index :companies, :created_at
+    add_index :companies, :employee_count
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_16_133042) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_16_145634) do
   create_table "companies", charset: "utf8mb4", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
     t.integer "employee_count"
     t.string "industry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_companies_on_created_at"
+    t.index ["employee_count"], name: "index_companies_on_employee_count"
+    t.index ["industry"], name: "index_companies_on_industry"
+    t.index ["name"], name: "index_companies_on_name"
   end
 
   create_table "deals", charset: "utf8mb4", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
